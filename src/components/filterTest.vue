49606cc0dec3c0ab6e4460392b1594ca6e48070d
katsuooo <katsuooo.g@gmail.com>
2021-06-12 21:16:05 +0900

    change port to 3010

--- src/components/filterTest.vue
+++ src/components/filterTest.vue
@@ -55,7 +55,7 @@ function getDatetime(){
 function getUrl(){
     //return io.request.connection.remoteAddress
     var web_server = window.location.host
-    var backend = web_server.split(':')[0] + ':3030'
+    var backend = web_server.split(':')[0] + ':3010'
     return backend
 }
 /**
